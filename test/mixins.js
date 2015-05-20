0b73e2e8658fb35f695438420b5c377eb9180284
Aratramba <aratramba@gmail.com>
2015-05-20 08:43:59 +0200

    separating files, fixes #12; working on single file generator

---
+++ test/mixins.js
@@ -0,0 +1,11 @@
+'use strict';
+/* globals require */
+
+var rimraf = require('rimraf');
+rimraf.sync('./test/tmp');
+
+var JadeDoc = require('../index');
+new JadeDoc({
+  input: './test/fixtures/**/*.jade',
+  output: './test/tmp/mixins.json'
+});
