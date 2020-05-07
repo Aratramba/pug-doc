fe28ea78d68531a441a8708986265023cfe45556
Aratramba <aratramba@gmail.com>
2020-05-07 09:30:20 +0200

    update dependencies

--- cli.js
+++ cli.js
@@ -5,32 +5,27 @@ var meow = require("meow");
 var pugDoc = require("./index");
 var JSONStream = require("JSONStream");
 
-var cli = meow({
-  help: [
+var cli = meow(`
     "Usage",
     "  $ pug-doc input.jade --output output.json",
     "",
     "Options",
     "  --output    Set output json file"
-  ]
-});
+  `);
 
 var jd = new pugDoc({
   input: cli.input[0],
-  output: cli.flags.output
+  output: cli.flags.output,
 });
 
-process.stdin
-  .pipe(jd)
-  .pipe(JSONStream.stringify())
-  .pipe(process.stdout);
+process.stdin.pipe(jd).pipe(JSONStream.stringify()).pipe(process.stdout);
 
-jd.on("complete", function() {
+jd.on("complete", function () {
   process.exit();
 });
 
 if (!cli.flags.output) {
-  jd.on("end", function() {
+  jd.on("end", function () {
     process.exit();
   });
 }
